ca694d80e18a74d81e449cece6d64efeb62ac047
Amar Mallick <amarmallick.sqa@gmail.com>
2024-09-26 13:27:11 +0530

    commit: check fibonacci series

---
+++ JAVA_Selenium_Code/src/test/java/javaprogramme/Day14_CheckFibonacciSeries.java
@@ -0,0 +1,19 @@
+package javaprogramme;
+
+public class Day14_CheckFibonacciSeries {
+   //Fibonacci Number>> current number is sum of previous/preceding two numbers.
+	public static void main(String[] args) {
+		int n1=0,n2=1,sum=0;
+		System.out.print(n1+" "+n2);
+		
+		 for (int i = 2; i < 10; i++) {
+			sum=n1+n2;
+			System.out.print(" "+ sum);
+			n1=n2;
+			n2=sum;
+			
+			
+		}
+	}
+
+}
