130dbebdec1837c1339ca11168a50e533c86f48b
Amar Mallick <amarmallick.sqa@gmail.com>
2024-09-30 19:45:59 +0530

    commit:Find Sum of Elements in an Array

---
+++ JAVA_Selenium_Code/src/test/java/javaprogramme/Day17_FindSumOfArrayElements.java
@@ -0,0 +1,15 @@
+package javaprogramme;
+
+public class Day17_FindSumOfArrayElements {
+
+	public static void main(String[] args) {
+		int arr[]= {2,5,3,1};
+		int sum=0;
+		for( int n:arr)
+		{
+			sum=sum+n;
+		}
+          System.out.println(sum);
+	}
+
+}
