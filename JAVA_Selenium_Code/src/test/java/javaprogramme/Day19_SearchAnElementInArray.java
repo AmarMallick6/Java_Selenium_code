e1d4377f2bb897fb60a88a3aa6a724326f012c2c
MAK <amarmallick6@gmail.com>
2024-10-14 14:18:28 +0530

    commit: Find Elements In Array (Linear Search)

--- JAVA_Selenium_Code/src/test/java/javaprogramme/Day19_SearchAnElementInArray.java
+++ JAVA_Selenium_Code/src/test/java/javaprogramme/Day19_SearchAnElementInArray.java
@@ -1,15 +1,15 @@
 package javaprogramme;
 
 public class Day19_SearchAnElementInArray {
-
+    //Using Linear Search Method means Sequentially check one after another...
 	public static void main(String[] args) {
 		int a[]= {12,30,50,70,75};
-		int search_element=705;
+		int search_element=75;
 		boolean staus=false;
 		for (int i : a) {
 			if(i==search_element) 
 			{
-				System.out.println("Element is present");
+				System.out.println("Element is present ,Element is:" + i);
 				staus=true;
 				break;
 			}
