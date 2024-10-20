8eb63a3facb6efe5722e15ddf0c4794e3c32b0ec
MAK <amarmallick6@gmail.com>
2024-10-20 21:07:20 +0530

    commit: Print System output without  using Semicolon(;)

---
+++ JAVA_Selenium_Code/src/test/java/javaprogramme/Day34_PrintHelloWithoutUsingSemicolon.java
@@ -0,0 +1,17 @@
+package javaprogramme;
+
+public class Day34_PrintHelloWithoutUsingSemicolon {
+
+	public static void main(String[] args) {
+		// Approach-01>>>>>>>>>>>>>>>>>>>>>>
+		if (System.out.printf("Hello"+"\t")==null) {
+			
+		}
+		// Approach-02>>>>>>>>>>>>>>>>>>>>>>
+		if (System.out.append("Welcome")==null) {
+			
+		}
+		
+	}
+
+}
